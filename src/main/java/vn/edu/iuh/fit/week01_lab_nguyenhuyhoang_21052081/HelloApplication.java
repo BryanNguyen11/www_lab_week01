40d0c5a5b1b7ac25a705f898513b087bf08c1ccc
Huy Hoang <huyhoang.ng203@gmail.com>
2024-09-18 10:02:41 +0700

    create Project, project structure

---
+++ src/main/java/vn/edu/iuh/fit/week01_lab_nguyenhuyhoang_21052081/HelloApplication.java
@@ -0,0 +1,9 @@
+package vn.edu.iuh.fit.week01_lab_nguyenhuyhoang_21052081;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
